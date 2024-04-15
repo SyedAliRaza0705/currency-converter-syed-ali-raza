1a3a25bb1aef4686dd8da3ed188eb9ae7e75358e
SyedAliRaza0705 <164862312+SyedAliRaza0705@users.noreply.github.com>
2024-04-15 22:56:30 +0500

    Currency Converter

---
+++ main.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+let exchange_rate = {
+    "USD": 1,
+    "JYP": 113,
+    "AUD": 1.65,
+    "PKR": 280 // Pakistani Rupees 
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: "Select your currency to convrt from",
+        choices: ["USD", "JYP", "AUD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "Select your currency to convrt into",
+        choices: ["USD", "JYP", "AUD", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message: "Enter your amount to convrt ",
+    }
+]);
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount;
+let baseAmount = amount / from_amount;
+let convertedAmount = baseAmount * to_amount;
+console.log(`Converted Amount = ${convertedAmount}`);
